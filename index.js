376226d7620739d5d66b02354acc923efa5b6821
Gustavo Dias <gustavo.silva@fiec.edu.br>
2026-03-10 16:28:01 -0300

    Create rota para exibir todos os usuarios

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const app = express();
 app.use(express.json()); // Habilita leitura de JSON no CORPO DA REQUISIÇÃO.
 app.use(cors()); // Permite que o FRONTEND  acesse esta API.
 
+
+// EXERCÍCIO 1
+// MIDDLEWARE PARA ACESSAR TODAS AS ROTAS
+app.use((req, res, next) => {
+  const dataAtual = new Date().toLocaleDateString('pt-BR');
+  console.log(`[${dataAtual}] Método: ${req.method} | URL: ${req.url}`);
+  next();
+})
+
+
+
+// EXERCÍCIO 2
+const validarEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  // Verifica se o email existe e contem o caractere "@"
+  if (!email || !email.includes('@')) {
+    console.log("⚠️ Bloqueio: Tentativa de cadastro com e-mail invalido!")
+    return res.status(400).json({ mensagem: "Email inválido! O campo deve conter '@' ."})
+  }
+  next();
+}
+
+
 // Configurar a CONEXÃO com o BD nesse caso é o MySQL
 const db = mysql.createConnection({
   host: 'localhost',
@@ -24,7 +48,7 @@ db.connect((err) => {
 })
 
 // --- ROTA DE CADASTRO ---
-app.post('/cadastrar', (req, res) => {
+app.post('/cadastrar', validarEmail, (req, res) => {
   const { nome, email, senha } = req.body;
 
   const sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
@@ -51,6 +75,15 @@ app.post('/login', (req, res) => {
     }
   });
 });
+
+// ROTA DE EXIBIR TODOS USUÁRIOS
+app.get('/usuarios',( req, res) => {
+  const sql = 'SELECT * FROM usuarios';
+  db.query(sql, (err, result) => {
+    if(err) return res.status(500).json(err);
+    res.status(200).json(result);
+  })
+})
 
 app.listen(3000, () => {
   console.log(`Deu certo!`)
